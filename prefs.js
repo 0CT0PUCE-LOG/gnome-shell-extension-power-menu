585761f22a4f8f9f78c40de44f1abc3076435fc8
starcks <soren.starck@etu.umontpellier.fr>
2023-04-09 13:41:05 +0200

    pref

--- prefs.js
+++ prefs.js
@@ -26,6 +26,22 @@ const MyPrefsWidget = new GObject.Class({
             label : "Translated Text",
         });
 
-        this.add(label);
+        let spinButton = new Gtk.SpinButton();
+        spinButton.set_sensitive(true);
+        spinButton.set_range(-60, 60);
+        spinButton.set_value(0);
+        spinButton.set_increments(1, 2);
+
+        spinButton.connect('value-changed', function (widget) {
+            log('spinButton value-changed: ' + widget.get_value());
+        });
+
+        let hBox = new Gtk.Box();
+        hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
+
+        hBox.pack_start(label, false, false, 0);
+        hBox.pack_end(spinButton, false, false, 0);
+
+        this.add(hBox);
     }
 });
