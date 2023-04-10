e8829710e23dafe7343887d76c9d1da24088c632
starcks <soren.starck@etu.umontpellier.fr>
2023-04-10 16:10:35 +0200

    change all indic and button names

--- extension.js
+++ extension.js
@@ -1,184 +1,94 @@
-/**********************************************************************
- *
- *   Extension created by :
- *   ___   _____ _______ ___  _____  _    _  _____ ______
- *  / _ \ / ____|__   __/ _ \|  __ \| |  | |/ ____|  ____|
- * | | | | |       | | | | | | |__) | |  | | |    | |__
- * | | | | |       | | | | | |  ___/| |  | | |    |  __|
- * | |_| | |____   | | | |_| | |    | |__| | |____| |____
- *  \___/ \_____|  |_|  \___/|_|     \____/ \_____|______|
- *
- * PowerMenu is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
- *
- **********************************************************************/
-
-
 const Gio = imports.gi.Gio;
+const St = imports.gi.St;
+
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-//const GETTEXT_DOMAIN = 'my-indicator-extension';
-const GETTEXT_DOMAIN = Me.metadata['gettext-domain'];
-
-const { GObject, St } = imports.gi;
-
-
-
 const Main = imports.ui.main;
-const GLib = imports.gi.GLib;
 const PanelMenu = imports.ui.panelMenu;
-const _ = ExtensionUtils.gettext;
 
-const SCHEMA_NAME = 'org.gnome.shell.extensions.power-menu';
-const schemaDir = Me.dir.get_child('schemas');
-const schemaSource = Gio.SettingsSchemaSource.new_from_directory(schemaDir.get_path(), Gio.SettingsSchemaSource.get_default(), false);
-const schema = schemaSource.lookup(SCHEMA_NAME, true);
-/*
-const DEFAULT_VALUES = {
-    mysetting: false,
-};
-
- */
-
-
-function getSettings() {
-    let GioSSS = Gio.SettingsSchemaSource;
-    let schemaSource = GioSSS.new_from_directory(
-        Me.dir.get_child('schemas').get_path(),
-        GioSSS.get_default(),
-        false
-    );
-    let schemaObj = schemaSource.lookup('org.gnome.shell.extensions.power-menu', true);
-    if (!schemaObj) {
-        throw new Error('cannot find schemas');
-    }
-    return new Gio.Settings({ settings_schema: schemaObj });
-}
-
-const Indicator = GObject.registerClass(
-    class Indicator extends PanelMenu.Button {
-        _init() {
-            super._init(0.0, _('My Shiny Indicator'));
-            //let settings = getSettings();
-            let button;
-
-            //log("test_boolean: " + settings.get_boolean("show-logout-button").toString());
-            //start prefs
-            //settings v1
-            /*
-            let Settings = ExtensionUtils.getSettings();
-            /*
-            Settings.connect('changed::panel-position', () => {
-                removeButton();
-                addButton();
-            });
-
-             */
-
-            /*
-            GLib.setenv('GSETTINGS_SCHEMA_DIR', Me.dir.get_child('schemas').get_path(), true);
-
-            let mysetting = false;
-
-            let settings = new Gio.Settings({
-                schema_id: 'org.gnome.shell.extensions.power-menu',
-            });
-
-            settings.connect('changed::my-setting', (settings, key) => {
-                mysetting = settings.get_boolean(key);
-            });
-
-
-            let mysettingtest = Settings.get_enum('my-setting');
-            */
-            //settings v2
-            this.settings = ExtensionUtils.getSettings(
-                'org.gnome.shell.extensions.power-menu');
-            let indicatorName = `${Me.metadata.name} Indicator`;
-
-            // Create a panel button
-            this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
-
-            // Add an icon
-            let icon = new St.Icon({
-                gicon: new Gio.ThemedIcon({name: 'face-laugh-symbolic'}),
-                style_class: 'system-status-icon'
-            });
-            this._indicator.add_child(icon);
-
-            // Bind our indicator visibility to the GSettings value
-            //
-            // NOTE: Binding properties only works with GProperties (properties
-            // registered on a GObject class), not native JavaScript properties
-            this.settings.bind(
-                'show-indicator',
-                this._indicator,
-                'visible',
-                Gio.SettingsBindFlags.DEFAULT
-            );
-
-            Main.panel.addToStatusArea(indicatorName, this._indicator);
-            //end prefs
-
-
-            //---------------------------OLD VERSION----------------------------------
-            //start button
-
-            //test label
-            /*
-            let testlabel = new St.Label({
-                text: mysettingtest.toString(),
-            });
-
-
-            button = new St.Bin({
-                style_class: "panel-button",
-                reactive: true,
-                can_focus: false,
-                track_hover: false,
-            });
-
-            let icon = new St.Icon({
-                icon_name: "system-log-out-symbolic",
-                style_class: "system-status-icon"
-            });
-
-            button.set_child(icon);
-
-            function forceLogout() {
-                let [success, pid] = GLib.spawn_async(null, ['gnome-session-quit', '--force'], null, GLib.SpawnFlags.SEARCH_PATH, null, null);
-            }
-
-            button.connect("button-press-event", forceLogout);
-
-            this.add_child(testlabel);
-            //end button
-
-             */
-        }
-    });
 
 class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
-        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
-    }
-    enable() {
-        //button.connect("button-press-event", forceLogout);
-        this._indicator = new Indicator();
-        Main.panel.addToStatusArea(this._uuid, this._indicator);
-    }
-    disable() {
-        //button.disconnect("button-press-event", forceLogout);
-        this._indicator.destroy();
+    constructor() {
         this._indicator = null;
+    }
+
+    enable() {
+        log(`enabling ${Me.metadata.name}`);
+
+        this.settings = ExtensionUtils.getSettings(
+            'org.gnome.shell.extensions.power-menu');
+
+        //building icons
+        let indic_logout = `${Me.metadata.name} indic_logout`;
+        let indic_reboot = `${Me.metadata.name} indic_reboot`;
+        let indic_shutdown = `${Me.metadata.name} indic_shutdown`;
+
+        // Create panel buttons
+        this._indic_logout = new PanelMenu.Button(0.0, indic_logout, false);
+        this._indic_reboot = new PanelMenu.Button(0.0, indic_reboot, false);
+        this._indic_shutdown = new PanelMenu.Button(0.0, indic_shutdown, false);
+
+        // Add icons
+        let icon_logout = new St.Icon({
+            icon_name: "system-log-out-symbolic",
+            style_class: "system-status-icon"
+        });
+        let icon_reboot = new St.Icon({
+            gicon: new Gio.ThemedIcon({name: 'face-laugh-symbolic'}),
+            style_class: 'system-status-icon'
+        });
+        let icon_shutdown = new St.Icon({
+            icon_name: "system-shutdown-symbolic",
+            style_class: "system-status-icon"
+        });
+
+        this._indic_logout.add_child(icon_logout);
+        this._indic_reboot.add_child(icon_reboot);
+        this._indic_shutdown.add_child(icon_shutdown);
+
+        // Bind our indicator visibility to the GSettings value
+        //
+        // NOTE: Binding properties only works with GProperties (properties
+        // registered on a GObject class), not native JavaScript properties
+        this.settings.bind(
+            'show-logout-button',
+            this._indic_logout,
+            'visible',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        this.settings.bind(
+            'show-reboot-button',
+            this._indic_reboot,
+            'visible',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        this.settings.bind(
+            'show-shutdown-button',
+            this._indic_shutdown,
+            'visible',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+
+        Main.panel.addToStatusArea(indic_logout, this._indic_logout);
+        Main.panel.addToStatusArea(indic_reboot, this._indic_reboot);
+        Main.panel.addToStatusArea(indic_shutdown, this._indic_shutdown);
+    }
+
+    disable() {
+        log(`disabling ${Me.metadata.name}`);
+
+        this._indic_logout.destroy();
+        this._indic_logout = null;
+        this._indic_reboot.destroy();
+        this._indic_reboot = null;
+        this._indic_shutdown.destroy();
+        this._indic_shutdown = null;
     }
 }
 
-function init(meta) {
-    let extension = new Extension(meta.uuid);
-    log('GETTEXT_DOMAIN:', GETTEXT_DOMAIN);
-    ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
-    return extension;
+
+function init() {
+    log(`initializing ${Me.metadata.name}`);
+
+    return new Extension();
 }
--- prefs.js
+++ prefs.js
@@ -12,7 +12,7 @@ function init() {
 function fillPreferencesWindow(window) {
     // Use the same GSettings schema as in `extension.js`
     const settings = ExtensionUtils.getSettings(
-        'org.gnome.shell.extensions.power-menu'
+        'org.gnome.shell.extensions.power-menu');
 
     // Create a preferences page and group
     const page = new Adw.PreferencesPage();
@@ -20,24 +20,56 @@ function fillPreferencesWindow(window) {
     page.add(group);
 
     // Create a new preferences row
-    const row = new Adw.ActionRow({ title: 'Show Extension Indicator' });
-    group.add(row);
+    const row_logout = new Adw.ActionRow({ title: 'Show logout button' });
+    group.add(row_logout);
+
+    const row_reboot = new Adw.ActionRow({ title: 'Show reboot button' });
+    group.add(row_reboot);
+
+    const row_shutdown = new Adw.ActionRow({ title: 'Show shutdown button' });
+    group.add(row_shutdown);
 
     // Create the switch and bind its value to the `show-indicator` key
-    const toggle = new Gtk.Switch({
-        active: settings.get_boolean ('show-indicator'),
+    const toggle_logout = new Gtk.Switch({
+        active: settings.get_boolean ('show-logout-button'),
+        valign: Gtk.Align.CENTER,
+    });
+    const toggle_reboot = new Gtk.Switch({
+        active: settings.get_boolean ('show-reboot-button'),
+        valign: Gtk.Align.CENTER,
+    });
+    const toggle_shutdown = new Gtk.Switch({
+        active: settings.get_boolean ('show-shutdown-button'),
         valign: Gtk.Align.CENTER,
     });
     settings.bind(
-        'show-indicator',
-        toggle,
+        'show-logout-button',
+        toggle_logout,
+        'active',
+        Gio.SettingsBindFlags.DEFAULT
+    );
+
+    settings.bind(
+        'show-reboot-button
+        toggle_reboot,
+        'active',
+        Gio.SettingsBindFlags.DEFAULT
+    );
+
+    settings.bind(
+        'show-shutdown-button
+        toggle_shutdown,
         'active',
         Gio.SettingsBindFlags.DEFAULT
     );
 
     // Add the switch to the row
-    row.add_suffix(toggle);
-    row.activatable_widget = toggle;
+    row_logout.add_suffix(toggle_logout);
+    row_logout.activatable_widget = toggle_logout;
+    row_reboot.add_suffix(toggle_reboot);
+    row_reboot.activatable_widget = toggle_reboot;
+    row_shutdown.add_suffix(toggle_shutdown);
+    row_shutdown.activatable_widget = toggle_shutdown;
 
     // Add our page to the window
     window.add(page);
